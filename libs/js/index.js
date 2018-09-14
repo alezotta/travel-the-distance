b1a748cc4c2c7eb26cfc91fa200be57f9fb7d83f
Alessandro Zotta <zotta.ale@gmail.com>
2018-09-14 14:23:44 +0200

    fix

--- libs/js/index.js
+++ libs/js/index.js
@@ -92,7 +92,7 @@ function navMenu() {
     if (x.className === "topnav" || "topnav affix-menu") {
         x.classList.add("responsive");
     } else {
-        x.className = "topnav affix-menu");
+        x.className = "topnav affix-menu";
     }
 }
 
